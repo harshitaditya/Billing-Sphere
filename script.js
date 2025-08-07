39ab8de01fb9fa5ef5d29ad3f6cefda8f6ed9dc4
harshitaditya <kumaraditya02072004@gmail.com>
2025-08-07 17:56:37 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+  function showTab(tabName) {
+    document.getElementById('asia').classList.add('hidden');
+    document.getElementById('other').classList.add('hidden');
+    document.querySelectorAll('.tab').forEach(t => t.classList.remove('active'));
+
+    if (tabName === 'asia') {
+      document.getElementById('asia').classList.remove('hidden');
+      document.querySelectorAll('.tab')[0].classList.add('active');
+    } else {
+      document.getElementById('other').classList.remove('hidden');
+      document.querySelectorAll('.tab')[1].classList.add('active');
+    }
+  }
+
+
